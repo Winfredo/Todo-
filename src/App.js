5173bebc63b961de4a88241657f26de98967ee8c
Winfred Nukpezah <winfrednukpe2002@gmail.com>
2022-08-14 22:46:48 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import logo from './logo.svg';
+
 
 function App() {
   return (
-    <h1 className="text-3xl text-red-200 font-bold underline">
-      Hello world!
-    </h1>
-
+   <div>winfred</div>
   );
 }
 
