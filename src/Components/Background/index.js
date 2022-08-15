1796a81ce0b7d2a263e911375f0dbf7fd4d9321c
Winfred Nukpezah <winfrednukpe2002@gmail.com>
2022-08-15 21:27:10 +0000

    commit

---
+++ src/Components/Background/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function BackgroundPage() {
+  return (
+    <div className='w-screen h-screen  bg-red-300 flex justify-center items-center'>
+      <div className='font-semibold text-4xl mt-10'>
+        <h1>Winfred Todo List</h1>
+      </div>
+      <div>
+        <form>
+          <label>
+            username
+          </label>
+        </form>
+      </div>
+
+    </div>
+    
+    
+  );
+}
+
+export default BackgroundPage;
