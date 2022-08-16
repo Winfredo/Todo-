93f7767ccc9311a9f67b04a14c62862af89f88fb
Winfredo <winfrednukpe2002@gmail.com>
2022-08-16 14:50:32 +0000

    Completed the input form.

--- src/Components/Background/index.js
+++ src/Components/Background/index.js
@@ -9,9 +9,9 @@ function BackgroundPage() {
       </div>
       <div className='w-screen flex items-center justify-center'>
         <form className=' w-[60%] '>
-          <div className='flex justify-center items-center gap-x-1 bg-white border border-red-500 w-full h-10 p-2'>
+          <div className='flex justify-between items-center gap-x-1 bg-white border border-red-500 w-full h-10 p-0'>
           <input className='w-[80%] h-8 outline-none ' type="text" placeholder="" />
-            <button className=' h-8 p w-[15%] shadow-xl hover:bg-[#90ad0d] bg-gray-800 flex items-center justify-center border cursor-pointer"'>
+            <button className=' h-10 p w-[15%] shadow-xl hover:bg-red-500 bg-red-400 flex items-center justify-center  cursor-pointer"'>
               <GrAddCircle />
             </button>
           </div>
