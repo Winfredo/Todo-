43202bf2416f655799fb4369d83ea4496e2bc8da
Winfredo <winfrednukpe2002@gmail.com>
2022-08-19 22:15:40 +0000

    Made some changes to the text.

--- src/Components/Background/index.js
+++ src/Components/Background/index.js
@@ -4,9 +4,9 @@ import InputPage from '../Input';
 
 function BackgroundPage() {
   return (
-    <div className='w-screen h-screen overflow-hidden md: bg-red-300 flex flex-col items-center'>
+    <div className='w-screen h-screen overflow-hidden md: bg-red-400 flex flex-col items-center'>
       <div className='font-semibold text-[3rem] m-14'>
-        <h1>Winfred Todo List</h1>
+        <h1>Winfred's Todo List</h1>
       </div>
       <div className='w-screen flex items-center justify-center'>
         <form className=' w-96 '>
