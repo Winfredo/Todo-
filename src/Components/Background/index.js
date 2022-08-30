54a9fd58714236414728e6ad33561ce752b5be0c
Winfredo <winfrednukpe2002@gmail.com>
2022-08-30 20:45:18 +0000

    Made some changes.

--- src/Components/Background/index.js
+++ src/Components/Background/index.js
@@ -13,7 +13,7 @@ function BackgroundPage() {
       <div className='w-screen flex items-center justify-center  '>
         <form className=' w-96 '>
           <div className='flex justify-between items-center gap-x-1 bg-white border border-red-500 w-full h-10 p-0'>
-          <input className='w-60 pl-2 h-8 outline-none ' type="text" placeholder="" />
+          <input className=' font-semibold w-60 pl-2 h-8 outline-none ' type="text" placeholder="Create a new todo" />
             <button className=' h-10  p w-[15%] shadow-xl hover:bg-red-500 bg-red-400 flex items-center justify-center  cursor-pointer"'>
               <GrAddCircle size={21}/>
             </button>
