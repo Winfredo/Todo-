5c1a559bed68f397ee42b5c6d5ab78e856c93cd4
Winfredo <winfrednukpe2002@gmail.com>
2022-08-30 22:20:56 +0000

    Made Changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
- import React from 'react'
+ import React, { useState } from 'react'
 import BackgroundPage from "./Components/Background";
 
 
 
 function App() {
+ 
   return (
    <div>
     < BackgroundPage />
@@ -15,3 +16,4 @@ function App() {
 }
 
 export default App;
+
