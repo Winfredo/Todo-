4669ee84557f05db0d80ee59f3b3fbf0a24433df
Winfredo <winfrednukpe2002@gmail.com>
2022-08-30 20:45:53 +0000

    Added some props.

--- src/Components/todo/index.js
+++ src/Components/todo/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const TodoPage =() => {
+const TodoPage =({text, deleteTodo, isChecked }) => {
   return (
     <div>
       {/* unit todo */}
       <div className="w-[80%]  flex items-center justify-center gap-2">
         <div className="w-[60%] bg-white py-2 px-3">
-          <p>Todo Texts</p>
+          <p className="h-6"> {text}</p>
         </div>
         <div className="w-[10%] flex items-center justify-center gap-2">
           <button className="bg-green-400 hover:bg-green-500 py-2 px-3 text-white text-base font-sans ">
