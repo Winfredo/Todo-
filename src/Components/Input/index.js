d226c9bd0d4c3b20876b99f9104f53a593b487b8
Winfredo <winfrednukpe2002@gmail.com>
2022-08-30 20:45:34 +0000

    Changes

--- src/Components/Input/index.js
+++ src/Components/Input/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function InputPage() {
   return (
-    <div className='grid grid-cols-3 mt-11 box-content items-center justify-between cursor-pointer'>
+    <div className='grid grid-cols-3 mt-10 box-content items-center justify-between cursor-pointer'>
      
       <button className='bg-blue-300 hover:bg-blue-400 mr-8 px-8 py-1' >
         ALL : 0
